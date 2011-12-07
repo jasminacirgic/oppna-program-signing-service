0021212b3c5921920622cf05042cb10abb1cc327
patrik.bergstrom@knowit.se <patrik.bergstrom@knowit.se@4fa5f3af-48f4-46d1-8f5e-f49c9857bd2e>
2011-12-07 16:07:07 +0000

    - started on verify signature - test for RestSignController with XMLDIGSIG and CMS signatures - added SignatureVerificationRequest_1.0.xsd to make JAXB entity

--- signer-service/trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/security/sign/RestSignController.java
+++ signer-service/trunk/core-bc/composites/webcomp/src/main/java/se/vgregion/security/sign/RestSignController.java
@@ -1,25 +1,50 @@
 package se.vgregion.security.sign;
 
+import org.bouncycastle.cms.CMSEnvelopedDataParser;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.util.encoders.Base64;
+import org.jcp.xml.dsig.internal.dom.DOMX509Data;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 import se.vgregion.dao.domain.patterns.repository.Repository;
 import se.vgregion.domain.security.pkiclient.ELegType;
+import se.vgregion.signera.signature._1.SignatureFormat;
+import se.vgregion.signera.signature._1.SignatureVerificationRequest;
 import se.vgregion.ticket.Ticket;
-import se.vgregion.ticket.TicketManager;
 import se.vgregion.ticket.TicketException;
+import se.vgregion.ticket.TicketManager;
 import se.vgregion.web.dto.TicketDto;
 import se.vgregion.web.security.services.SignatureData;
 import se.vgregion.web.security.services.SignatureService;
+import sun.security.x509.X509CertImpl;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
+import javax.xml.crypto.KeySelector;
+import javax.xml.crypto.MarshalException;
+import javax.xml.crypto.dom.DOMStructure;
+import javax.xml.crypto.dsig.XMLSignature;
+import javax.xml.crypto.dsig.XMLSignatureException;
+import javax.xml.crypto.dsig.XMLSignatureFactory;
+import javax.xml.crypto.dsig.dom.DOMValidateContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.PublicKey;
 import java.security.SignatureException;
 import java.util.Collection;
 
@@ -75,9 +100,45 @@ public class RestSignController extends AbstractSignController {
      * 
      * @see AbstractSignController#verifySignature(SignatureData)
      */
-    @Override
-    public boolean verifySignature(@RequestBody SignatureData signData) throws SignatureException {
-        return super.verifySignature(signData);
+//    @Override
+    @POST
+    @Path("/verifySignature")
+    @Consumes("application/xml")
+    @Produces("text/plain")
+    public String verifySignature(SignatureVerificationRequest signatureVerificationRequest) throws SignatureException, CMSException, IOException, ParserConfigurationException, SAXException, MarshalException, XMLSignatureException {
+
+        SignatureFormat format = signatureVerificationRequest.getSignatureFormat();
+        if (SignatureFormat.XMLDIGSIG.equals(format)) {
+            String signature = signatureVerificationRequest.getSignature();
+
+            InputStream is = new ByteArrayInputStream(signature.getBytes());
+
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setNamespaceAware(true);
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse(is);
+
+            XMLSignature xmlSignature = XMLSignatureFactory.getInstance().unmarshalXMLSignature(new DOMStructure(document));
+            xmlSignature.getKeyInfo().getContent();
+            PublicKey publicKey = ((X509CertImpl) ((DOMX509Data) (xmlSignature.getKeyInfo()).getContent().get(0)).getContent().get(0)).getPublicKey();
+            DOMValidateContext validateContext = new DOMValidateContext(KeySelector.singletonKeySelector(publicKey), document);
+            boolean validate = xmlSignature.validate(validateContext);
+
+//        CMSEnvelopedDataParser envelopedDataParser = new CMSEnvelopedDataParser(signature.getBytes());
+//        CMSSignedData cmsSignedData = new CMSSignedData(signature.getBytes());
+//        X509CertParser certParser = new X509CertParser();
+
+//        super.verifySignature()
+        } else if (SignatureFormat.CMS.equals(format)) {
+            String signature = signatureVerificationRequest.getSignature();
+            byte[] decoded = Base64.decode(signature);
+            System.out.println(new String(decoded));
+            org.apache.commons.codec.binary.Base64.decodeBase64(signature);
+            CMSEnvelopedDataParser parser = new CMSEnvelopedDataParser(decoded);
+            System.out.println(parser);
+        }
+        return "hej";
+//        return super.verifySignature(signData);
     }
 
     /**
--- signer-service/trunk/core-bc/composites/webcomp/src/test/java/se/vgregion/security/sign/RestSignControllerTest.java
+++ signer-service/trunk/core-bc/composites/webcomp/src/test/java/se/vgregion/security/sign/RestSignControllerTest.java
@@ -1,14 +1,24 @@
 package se.vgregion.security.sign;
 
+import org.apache.cxf.helpers.IOUtils;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 import se.vgregion.dao.domain.patterns.repository.Repository;
+import se.vgregion.signera.signature._1.SignatureFormat;
+import se.vgregion.signera.signature._1.SignatureVerificationRequest;
 import se.vgregion.ticket.Ticket;
 import se.vgregion.ticket.TicketException;
 import se.vgregion.ticket.TicketManager;
@@ -17,13 +27,17 @@ import se.vgregion.web.security.services.ServiceIdService;
 import se.vgregion.web.security.services.SignatureService;
 
 import javax.ws.rs.core.Response;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.ParseException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -58,6 +72,81 @@ public class RestSignControllerTest {
         sf.setResourceProvider(RestSignController.class, new SingletonResourceProvider(controller));
         sf.setAddress(baseAddress);
         sf.create();
+    }
+
+    @Test
+    public void testVerifySignatureWithXmlDigSig() throws IOException, JAXBException {
+
+        //Read a signature
+        InputStream signatureStream = this.getClass().getClassLoader().getResourceAsStream("X509Signature.txt");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IOUtils.copy(signatureStream, baos);
+
+        //Create the SignatureVerificationRequest
+        SignatureVerificationRequest request = new SignatureVerificationRequest();
+        request.setSignature(baos.toString());
+        request.setSignatureFormat(SignatureFormat.XMLDIGSIG);
+
+        //Marshal the SignatureVerificationRequest
+        JAXBContext jc = JAXBContext.newInstance(SignatureVerificationRequest.class);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        jc.createMarshaller().marshal(request, os);
+
+        //Set headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        HttpEntity<String> entity = new HttpEntity<String>(os.toString(), headers);
+
+        RestTemplate template = new RestTemplate();
+
+        //Create proxy
+        SimpleClientHttpRequestFactory factory = (SimpleClientHttpRequestFactory) template.getRequestFactory();
+        factory.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8888)));
+
+        ResponseEntity<String> response = template.exchange(baseAddress + "/verifySignature", HttpMethod.POST,
+                entity, String.class);
+
+        String body = response.getBody();
+
+        System.out.println(body);
+
+    }
+    @Test
+    public void testVerifySignatureWithCms() throws IOException, JAXBException {
+
+        //Read a signature
+        InputStream signatureStream = this.getClass().getClassLoader().getResourceAsStream("CMSSignature.txt");
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        IOUtils.copy(signatureStream, baos);
+
+        //Create the SignatureVerificationRequest
+        SignatureVerificationRequest request = new SignatureVerificationRequest();
+        request.setSignature(baos.toString());
+        request.setSignatureFormat(SignatureFormat.CMS);
+
+        //Marshal the SignatureVerificationRequest
+        JAXBContext jc = JAXBContext.newInstance(SignatureVerificationRequest.class);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        jc.createMarshaller().marshal(request, os);
+
+        //Set headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        HttpEntity<String> entity = new HttpEntity<String>(os.toString(), headers);
+
+        RestTemplate template = new RestTemplate();
+
+        //Create proxy
+        SimpleClientHttpRequestFactory factory = (SimpleClientHttpRequestFactory) template.getRequestFactory();
+        factory.setProxy(new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 8888)));
+
+        ResponseEntity<String> response = template.exchange(baseAddress + "/verifySignature", HttpMethod.POST,
+                entity, String.class);
+
+        String body = response.getBody();
+
+        System.out.println(body);
+
     }
 
     @Test
